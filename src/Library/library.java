cab5b3792593943ab7e0b41c9581f7bcd334f7a0
adoniagracio <40045910+adoniagracio@users.noreply.github.com>
2023-01-06 13:22:01 +0700

    rdvftbg
    sembarang

---
+++ src/Library/library.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Library;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author adoni
+ */
+public class library {
+    public static void main(String[] args){
+        new Log_Form().show();
+}
+}
